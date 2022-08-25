4f9c310a49610fe75199989afd992dab1d5dd995
mikeydavis123 <110591072+mikeydavis123@users.noreply.github.com>
2022-08-25 06:42:09 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("ham").addEventListener("click",open);
+function open(){
+document.getElementById("menu").classList.toggle("active")
+document.getElementById("x1").classList.toggle("ac")
+document.getElementById("x2").classList.toggle("ac1")
+document.getElementById("x3").classList.toggle("ac2")
+}
+console.log("hello")
